b0d45c06ececc4be91bd471d06154b880d578760
Enes <enesyerebakan7@gmail.com>
2025-01-21 00:13:12 +0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Policy from './components/Policy'
+
+function App() {
+
+  return (
+    <div>
+
+      <Header/>
+      <h1>Hello</h1>
+      <Policy />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
